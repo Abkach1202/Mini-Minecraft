47e6342d3224aa643e17559193813eb771f24882
mamoudoudiaby <mamoudoudiaby8376@gmail.com>
2024-02-22 21:20:37 +0100

    Fixation du bug de rotation de porte

--- Minecraft/Assets/Scripts/Interactions/Door/DoorMovement.cs
+++ Minecraft/Assets/Scripts/Interactions/Door/DoorMovement.cs
@@ -6,14 +6,16 @@ public class DoorMovement : MonoBehaviour
     [SerializeField] private GameObject Pivot, Door;
     // Maximum Angle of rotation
     [SerializeField] private int Angle;
+    // Speed of rotation
+    [SerializeField] private float RotationSpeed;
     // The current angle of the rotation
-    private int CurrentAngle;
+    private float CurrentAngle;
     // If the player is detected
     private bool PlayerDetected;
 
     void Start()
     {
-        this.CurrentAngle = 0;
+        this.CurrentAngle = 0f;
         this.PlayerDetected = false;
     }
 
@@ -21,17 +23,18 @@ public class DoorMovement : MonoBehaviour
     {
         if (PlayerDetected && CurrentAngle < Angle)
         {
-            CurrentAngle += 1;
-            Door.transform.RotateAround(Pivot.transform.position, Vector3.up, CurrentAngle * Time.deltaTime);
+            CurrentAngle += RotationSpeed * Time.deltaTime;
+            Door.transform.RotateAround(Pivot.transform.position, Vector3.up, RotationSpeed * Time.deltaTime);
         }
-        else if (!PlayerDetected && CurrentAngle > 0)
+        else if (!PlayerDetected && CurrentAngle > 0f)
         {
-            CurrentAngle -= 1;
-            Door.transform.RotateAround(Pivot.transform.position, -Vector3.up, CurrentAngle * Time.deltaTime);
+            CurrentAngle -= RotationSpeed * Time.deltaTime;
+            Door.transform.RotateAround(Pivot.transform.position, -Vector3.up, RotationSpeed * Time.deltaTime);
         }
         
-        if (CurrentAngle == 0)
+        if (CurrentAngle <= 0f)
         {
+            CurrentAngle = 0f;
             Door.transform.rotation = Quaternion.Euler(0, 180, 0);
             Door.transform.localPosition = Vector3.zero;
         }
